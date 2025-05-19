811b33b7c64c8367c3a47374858afe6696e0a61f
p0t4t0sandwich <p0t4t0sandwich@gmail.com>
2025-05-19 03:46:36 -0600

    Updated packages and tossed out the WIP twitch stuff for now

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require github.com/bwmarrin/discordgo v0.28.1
 
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
-	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/net v0.39.0 // indirect
-	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"github.com/NeuralNexusDev/neuralnexus-discord-bot/src/discord"
+	"github.com/NeuralNexusDev/neuralnexus-discord-bot/src/discord/modules/bng"
+	"github.com/NeuralNexusDev/neuralnexus-discord-bot/src/discord/modules/gss"
+	"github.com/NeuralNexusDev/neuralnexus-discord-bot/src/discord/modules/mcstatus"
 )
 
 func main() {
 	discordBot := discord.NewBot()
-	// discordBot.AddCommandHandler(gss.GSSCommand, gss.GSSHandler)
-	// discordBot.AddCommandHandler(mcstatus.MCStatusCommand, mcstatus.MCStatusHandler)
-	// discordBot.AddCommandHandler(bng.BeeNameCommand, bng.BeeNameCommandHandler)
-	// discordBot.AddComponentHandlers(bng.BeeNameComponentHandlers)
+	discordBot.AddCommandHandler(gss.GSSCommand, gss.GSSHandler)
+	discordBot.AddCommandHandler(mcstatus.MCStatusCommand, mcstatus.MCStatusHandler)
+	discordBot.AddCommandHandler(bng.BeeNameCommand, bng.BeeNameCommandHandler)
+	discordBot.AddComponentHandlers(bng.BeeNameComponentHandlers)
 	discordBot.Start()
 }
